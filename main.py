bd437a622efea949df09426177d9a727274447ff
Darabat <aisushin@gmail.com>
2019-03-09 22:43:20 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-#seems to be imported the wrong way
 import course
 
 levelFile = open("level.cdt","r")
 
-level = Course()
+level = course.Course()
 level.load(levelfile)
 
 print(level.toAIString())
