02b96599066651d405585e7628f102a02f281aa2
Darabat <aisushin@gmail.com>
2019-03-21 14:40:18 -0300

    Generated level output code
    Yet to be tested

--- assemble.py
+++ assemble.py
@@ -1,3 +1,17 @@
 import course
 import sys
 
+levelFile = open(sys.argv[1],"rb")
+
+level = course.Course()
+level.load(levelFile.read())
+
+stringFile = open(sys.argv[2],"r")
+
+lvlstr = stringFile.read()
+
+level.fromAIString(lvlstr)
+
+finalFile = open(sys.argv[3],"wb")
+
+finalFile.write(level.save())
