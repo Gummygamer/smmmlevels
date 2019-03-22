8605234aa6bf58335e8277a1ce7ee186507a0344
Darabat <aisushin@gmail.com>
2019-03-22 19:06:34 -0300

    Update course.py

--- course.py
+++ course.py
@@ -167,7 +167,7 @@ class Course:
             for j in range(0,26):
                 present = False
                 for spr in self.sprites:
-                    if (math.floor(spr.objx / 8) == i) and (math.floor(spr.objy / 8) == j):
+                    if (math.floor(spr.objx / 16) == i) and (math.floor(spr.objy / 16) == j):
                         aiString += spr.toAIChar()
                         present = True
                 if (present == False):
@@ -194,8 +194,8 @@ class Course:
         for c in lvlstr:
             if ord(c) != 32:
                 s = SpriteItem()
-                s.objx = math.floor((pos / 27)) * 8 
-                s.objy = (pos % 27) * 8
+                s.objx = math.floor((pos / 27)) * 16
+                s.objy = (pos % 27) * 16
                 s.type = max(ord(c) - 32, 0)
                 print(s.type)
                 if s.type > 0:
